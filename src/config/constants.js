245116d1b3dc2a7ca6b9a10bd8c80fb3344e778a
EstebanR05 <e05072003@gmail.com>
2025-02-25 20:41:42 -0500

    se integra nueva url del bcak

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:4000/api';
+// export const API_BASE_URL = 'http://localhost:4000/api';
+export const API_BASE_URL = 'https://productsbackend.onrender.com/api';
 
 export const ENDPOINTS = {
     PRODUCTS: `${API_BASE_URL}/products`,
